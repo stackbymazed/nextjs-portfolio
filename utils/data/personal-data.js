6b426c6f67b28958f672aea69a09fcaeaa6ef4a8
Nayem783982 <majedulislam223311@gmail.com>
2026-01-19 18:15:03 +0600

    createing the existed repo to polished portfolio

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Mazedul Islam",
+  profile: '/profilePicture.png',
+  designation: "Frontend Developer",
+  description: "My name is Mazedul Islam. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'majedulislam223311@gmail.com',
+  phone: '+8801893679303',
+  address: 'Feni ,sadar hospital,',
+  github: 'https://github.com/stackbymazed',
+  facebook: 'https://www.facebook.com/majedulislam.nayem',
+  linkedIn: 'https://www.linkedin.com/in/mazedul-islam22',
+  twitter: 'https://twitter.com',
+  stackOverflow: 'https://stackoverflow.com',
+  leetcode: "https://leetcode.com/u/Majedul_783982",
+  devUsername: "StackByMazed",
+  resume: "https://drive.google.com/file/d/13SVUHkWFsS3yLFGr_sVuyQ6-mahWX0EJ/view?usp=sharing"
+}
